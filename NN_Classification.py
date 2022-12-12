69eb1a65a919a9941b982901cbd6563e0974079c
Jan Schiefer <janschiefer@posteo.de>
2022-12-12 13:21:44 +0100

    Added basic plotting functionality to the NN classifier

--- NN_Classification.py
+++ NN_Classification.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from data_import import import_csv
 from Classification import Classification
+import matplotlib.pyplot as plt
 
 
 class NN_Classification(Classification):
@@ -19,7 +20,7 @@ class NN_Classification(Classification):
         self.get_model()
 
         # train the neural network
-        self.model.fit(self.x_train, self.y_train, epochs=self.epochs)
+        self.history = self.model.fit(self.x_train, self.y_train, validation_split=0.2, epochs=self.epochs)
 
         # testing the network on the testing data
         self.model.evaluate(self.x_test, self.y_test, verbose=2)
@@ -33,10 +34,30 @@ class NN_Classification(Classification):
         ])
         self.model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
+    def plot(self, param=0):
+        """
+        Plot the results/history from the classification
+        :param param: Which plots to display: 0=all, 1=accuracy, 2=loss
+        :return:
+        """
+        if param in [0, 1]:
+            plt.plot(self.history.history["accuracy"])
+            plt.plot(self.history.history["val_accuracy"])
+        if param in [0, 2]:
+            plt.plot(self.history.history["loss"])
+            plt.plot(self.history.history["val_loss"])
+        plt.legend(["training accuracy", "testing accuracy", "train-loss", "test-loss"], loc="best")
+        plt.xlabel("epoch")
+        plt.ylabel("accuracy/loss")
+        plt.title("model accuracy & loss")
+        plt.grid()
+        plt.show()
+
 
 def main(file):
     data = import_csv(file)
     classifier = NN_Classification(data)
+    classifier.plot(1)
 
 
 if __name__ == "__main__":
