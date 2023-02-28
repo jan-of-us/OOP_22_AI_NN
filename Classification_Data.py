bfa7614277a6da2d961688937a3cd8df404104a0
Jan Schiefer <janschiefer@posteo.de>
2023-02-28 15:23:28 +0100

    Turned into dataclass

--- Classification_Data.py
+++ Classification_Data.py
@@ -1,24 +1,38 @@
+from dataclasses import dataclass
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+@dataclass
 class Classification_Data:
-    def __init__(self, data, test_size=0.2, x_labels=None, y_label=None, hidden_layers=[64, 64], training_epochs=10,
-                 activation_func="relu", validation_split=True, trees=100):
-        # pd.dataframe: data
-        self.data = data
-        # Float: Share/Percentage of Data used for testing
-        self.test_size = test_size
-        # list of strings: with x_labels, if None all will be used
-        self.x_labels = x_labels
-        # string: title of column with labels for classification, if None last column will be used
-        self.y_label = y_label
-        # Array: number and nodes for hidden layers as array 3 layers with 64 nodes each: [64, 64, 64]
-        self.hidden_layers = hidden_layers
-        # Int: number of training epochs
-        self.training_epochs = training_epochs
-        # String: activation functions from tf.keras.activations
-        self.activation_func = activation_func
-        # Bool: Whether during the training a part of the data will already be used for testing after each epoch,
-        # needed for accuracy/loss per epoch graphs
-        self.validation_split = validation_split
-        # Int: Number of trees
-        self.trees = trees
+    # INPUTS
+    # data
+    data: pd.dataframe
+    # Share/Percentage of Data used for testing
+    test_size: float = 0.2
+    # with x_labels, if None all will be used
+    x_labels: str = None
+    # title of column with labels for classification, if None last column will be used
+    y_label: str = None
+    # number and nodes for hidden layers as array 3 layers with 64 nodes each: [64, 64, 64]
+    hidden_layers: list[int] = (64, 64)
+    # number of training epochs
+    training_epochs: int = 10
+    # activation functions from tf.keras.activations
+    activation_func: str = "relu"
+    # Whether during the training a part of the data will already be used for testing after each epoch,
+    # needed for accuracy/loss per epoch graphs
+    validation_split: bool = True
+    # Number of trees
+    trees: int = 100
+
+    # OUTPUTS
+    # Plots
+    confusion_matrix = None
+    accuracy_per_epoch = None
+    feature_importance = None
+
+    # text based
+    result_string = None
 
 
